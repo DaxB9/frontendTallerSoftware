90022cb408f412b9e7b7c0577d20ec5a3483f96d
Raul Alejandro Choque Valda <raul.choque@ucb.edu.bo>
2023-04-24 02:07:38 -0400

    Agregando el listado de publicaciones

--- src/app/components/recommended-post/recommended-post.component.ts
+++ src/app/components/recommended-post/recommended-post.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./recommended-post.component.css']
 })
 export class RecommendedPostComponent {
+  //Datos de prueba
+  posts = [{idPost:1,image:"https://picsum.photos/200",title:"titulo post", description:"descripcion post", fecha: "01/01/2023"},{idPost:2,image:"https://picsum.photos/200",title:"titulo post2", description:"descripcion post2", fecha: "01/01/2023"},{idPost:3,image:"https://picsum.photos/200",title:"titulo post3", description:"descripcion post3", fecha: "01/01/2023"}];
 
 }
