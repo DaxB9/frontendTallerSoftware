8002cefe4564de13c4c1861cf583c5d9365034aa
Marcelo Miranda <cmgm274@gmail.com>
2023-04-27 15:54:34 -0400

    Agregado de componentes y vista de select option

--- src/app/components/select-post/select-post.component.ts
+++ src/app/components/select-post/select-post.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {MatDialog} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-select-post',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./select-post.component.css']
 })
 export class SelectPostComponent {
+  constructor(public dialog: MatDialog) {}
+
+  openDialog() {
+    const dialogRef = this.dialog.open(DialogContentExampleDialog);
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
 
 }
+export class DialogContentExampleDialog {}
