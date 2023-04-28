e806763471f9623d40025b9608b82b4ad33e8e0f
Ignacio <josignaciol89@gmail.com>
2023-04-28 00:33:54 -0400

    integracion de recommended

--- src/app/components/recommended-post/recommended-post.component.ts
+++ src/app/components/recommended-post/recommended-post.component.ts
@@ -1,3 +1,4 @@
+import { RecommendedService } from 'src/app/services/recommended.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,18 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recommended-post.component.css']
 })
 export class RecommendedPostComponent implements OnInit{
-  //Datos de prueba
-  posts = [{idPost:1,image:"https://picsum.photos/200",title:"titulo post", description:"descripcion post", fecha: "01/01/2023"},
-  {idPost:2,image:"https://picsum.photos/200",title:"titulo post2", description:"descripcion post2", fecha: "01/01/2023"},
-  {idPost:3,image:"https://picsum.photos/200",title:"titulo post3", description:"descripcion post3", fecha: "01/01/2023"},
-  {idPost:3,title:"titulo post3", description:"descripcion post3", fecha: "01/01/2023"}];
-  public page!: number;
 
-  constructor(){
-
+  constructor(private recommended: RecommendedService){
+    console.log('El componente se a creado');
   }
+
+
+  solicitudId:number = 123465;
 
   ngOnInit(): void{
-
+    console.log('El componente se ha inicializado');
+      this.recommended.getRecomendaciones(this.solicitudId)
+      .subscribe(Response => {
+        this.posts = Response
+     });
   }
+  
+  //Datos de prueba
+
+  posts: any = [];
+
+  // posts = [{idPost:1,image:"https://picsum.photos/200",title:"titulo post", description:"descripcion post", fecha: "01/01/2023"},
+  // {idPost:2,image:"https://picsum.photos/200",title:"titulo post2", description:"descripcion post2", fecha: "01/01/2023"},
+  // {idPost:3,image:"https://picsum.photos/200",title:"titulo post3", description:"descripcion post3", fecha: "01/01/2023"},
+  // {idPost:3,title:"titulo post3", description:"descripcion post3", fecha: "01/01/2023"}];
+   public page!: number;
+
+  
+
+  
 }
