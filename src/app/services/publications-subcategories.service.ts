40ac35dc14fe54099bf829904d98d03b93d2cdb0
NOLJ21 <nikolai.lopez@ucb.edu.bo>
2023-05-05 21:45:38 -0400

    crecion de service para obtener y mandar subcategorias al backend en conjunto con el model subcategory

---
+++ src/app/services/publications-subcategories.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { SubCategory } from '../models/subCategory';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PublicationsSubcategoriesService { 
+
+  constructor(private http:HttpClient) { }
+  
+  private apiUrlGet = 'http://localhost:8080/api/subinterest/all'; 
+  // create the route for the backend to get the subcategories and put it in the model SubCategory, it has to be a list of subcategories because the backend returns a list of subcategories
+  getSubCategories() {
+    return this.http.get<SubCategory[]>(this.apiUrlGet);
+  }
+  
+}
