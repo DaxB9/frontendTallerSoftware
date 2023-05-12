8bed713e3c62a63a0194824bc271641ef442ccea
Raul Alejandro Choque Valda <raul.choque@ucb.edu.bo>
2023-05-11 23:14:46 -0400

    Agregando ventana de confirmacion para editar el perfil de usuario

--- src/app/components/ed-profile/ed-profile.component.ts
+++ src/app/components/ed-profile/ed-profile.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
 import { MatDialog } from '@angular/material/dialog';
 import { ModalInterestComponent } from './modal-interest/modal-interest.component';
+import Swal from'sweetalert2';
 
 @Component({
   selector: 'app-ed-profile',
@@ -23,5 +24,33 @@ export class EdProfileComponent {
   constructor(private matDialog:MatDialog){}
   openEdit(){
     this.matDialog.open(ModalInterestComponent);
+  }
+
+  async confirmationPerfilUpdate(){
+    Swal.fire({
+      title: 'Confirmar Actualizacion',
+      text: '¿Está seguro de modificar la informacion del usuario?',
+      icon: 'question',
+      showCancelButton: true,
+      confirmButtonText: 'Confirmar',
+      cancelButtonText: 'Cancelar',
+    }).then(async (result) => {
+      if (result.value) {
+
+        //await this.updateProfile(profile);
+
+        this.successUpdateProfile();
+      }
+    })
+  }
+
+  successUpdateProfile(){
+    Swal.fire({
+      title: 'Exito',
+      text: 'Perfil actualizado correctamente',
+      icon: 'success',
+      showCancelButton: false,
+      confirmButtonText: 'Ok',
+    })
   }
 }
