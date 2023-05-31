d892385f00ed8f1e746710c1b63cf73b8e7669e9
NOLJ21 <nikolai.lopez@ucb.edu.bo>
2023-05-31 17:48:31 -0400

    arreglos finales

--- src/app/components/mispublicaciones/mispublicaciones.component.ts
+++ src/app/components/mispublicaciones/mispublicaciones.component.ts
@@ -1,27 +1,54 @@
 import { Component } from '@angular/core';
+import { PanelService } from 'src/app/services/panel.service';
+import { PreviewService } from 'src/app/services/preview.service';
+import { OnInit } from '@angular/core';
+import { Category } from 'src/app/models/category';
+import { FileSend } from 'src/app/models/file';
+import { ModificacionPubliService } from 'src/app/services/modificacion-publi.service';
 
 @Component({
   selector: 'app-mispublicaciones',
   templateUrl: './mispublicaciones.component.html',
   styleUrls: ['./mispublicaciones.component.css']
 })
-export class MispublicacionesComponent {
-  publicaciones: any[] = [
-    {
-      titulo: 'Publicación 1',
-      descripcion: 'Descripción de la publicación 1',
-    },
-    {
-      titulo: 'Publicación 2',
-      descripcion: 'Descripción de la publicación 2',
-    },
+export class MispublicacionesComponent implements OnInit {
+  publicaciones: any = [];
+  titulo: string = "";
+  subtitulo: string = "";
+  descripcion: string = "";
+  selectedModality: string= "";
+  selectedScope: string = "";
+  publi: any = [];
 
-  ];
+  category: Category[]=[];
+  categoryaux: Category[]=[];
+
+  categoryaux2: any[]=[];
+  publicoAux!:string;
+  lldata!: string;
+
+  image!:any;
+  file!:FileSend;
+  cardImageBase64!: string;
+
+  imageid!:string;
+
+  constructor(private panelService: PanelService, private previewService: PreviewService, private modificacionService: ModificacionPubliService) { }
+
+  ngOnInit(): void {
+    this.panelService.GetSolicitud().subscribe(Response => {
+      this.publicaciones = Response;
+      console.log(this.publicaciones);
+    });
+  }
   publicacionSeleccionada: any;
   modalVisible: boolean = false;
 
-  abrirModal(publicacion: any) {
-    this.publicacionSeleccionada = publicacion;
+  abrirModal(publicacionid: bigint) {
+    this.previewService.getById(publicacionid).subscribe(Response => {
+    this.publi = Response;
+    console.log(this.publi);
+    });
     this.modalVisible = true;
   }
 
@@ -63,4 +90,20 @@ export class MispublicacionesComponent {
     }
   }
 
+  obtenerFecha1(timestamp: Date): string {
+    const date = new Date(timestamp);
+    const year = date.getFullYear();
+    const month = ('0' + (date.getMonth() + 1)).slice(-2);
+    const day = ('0' + date.getDate()).slice(-2);
+    return `${year}-${month}-${day}`;
+  }
+
+  guardarCambios() {
+    const titulo = this.publicacionSeleccionada.titulo;
+
+    this.modificacionService.putPublicacion(titulo).subscribe(Response => {
+      console.log(Response);
+      this.modalVisible = false;
+    });
+  }
 }
